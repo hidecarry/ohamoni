24ac9306efe91503767e203b6bc0878214a19302
hide-stream <hidestream28@gmail.com>
2025-03-07 20:58:29 +0900

    login画面

--- src/main.tsx
+++ src/main.tsx
@@ -3,12 +3,14 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import LandingPage from "./ui/landing-page/LandingPage.tsx";
 import { BrowserRouter, Route, Routes } from "react-router";
+import LoginPage from "./ui/login/LoginPage.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<LandingPage />} />
+        <Route path="/login" element={<LoginPage />} />
       </Routes>
     </BrowserRouter>
   </StrictMode>
