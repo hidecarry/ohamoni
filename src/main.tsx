34517190f0f56aac1c89dca2507529ede316de0b
hide-stream <hidestream28@gmail.com>
2025-03-07 20:51:45 +0900

    ファイル修正

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,14 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import App from "./LandingPage.tsx";
+import LandingPage from "./ui/LandingPage.tsx";
 import { BrowserRouter, Route, Routes } from "react-router";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<App />} />
+        <Route path="/" element={<LandingPage />} />
       </Routes>
     </BrowserRouter>
   </StrictMode>
