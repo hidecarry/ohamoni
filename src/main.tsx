7a038e476a81ff79f615a1f2ce6bbc618b3e9bbc
hide-stream <hidestream28@gmail.com>
2025-03-07 20:46:25 +0900

    tailwind追加

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import App from "./App.tsx";
+import App from "./LandingPage.tsx";
 import { BrowserRouter, Route, Routes } from "react-router";
 
 createRoot(document.getElementById("root")!).render(
