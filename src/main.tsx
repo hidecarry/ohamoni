86d0f27507cd89b724fbea788fb39de45ffd589a
hid3h <razyetts@hotmail.co.jp>
2025-03-17 16:01:03 +0900

    多分不要です。

--- src/main.tsx
+++ src/main.tsx
@@ -5,9 +5,6 @@ import LandingPage from "./ui/landing-page/LandingPage.tsx";
 import { BrowserRouter, Route, Routes } from "react-router";
 import LoginPage from "./ui/login/LoginPage.tsx";
 
-// 環境変数からサイトタイトルを設定
-document.title = import.meta.env.VITE_SITE_TITLE || "おはもに";
-
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <BrowserRouter>
